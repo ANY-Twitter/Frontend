d655aaaf3dd123563eb19f455aef3ea761370895
Eric <eric.bracamonte@utec.edu.pe>
2023-06-10 09:56:47 -0500

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from './components/Inicio'
 
 
