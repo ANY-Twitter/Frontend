d91787f1a7dee2c49b319583da991a7f897f64d2
Prober76 <ugartegrover@gmail.com>
2023-06-12 21:33:27 -0500

    fetch funcionando

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -8,22 +8,31 @@ function Inicio(){
 
     useEffect(()=>console.log(correo), [correo])
 
-    function Inicio_iniciar_sesion(){
-        window.location = "/login"
+    async function Inicio_iniciar_sesion(){
+        let resp = await fetch("http://127.0.0.1:8000/usuarios", {
+            'method':'POST',
+            'body': JSON.stringify({
+                'correo': correo,
+                'clave': clave
+            }),
+            'headers': {
+                'Content-Type': "application/json"
+            }
+        });
+        let texto = await resp.text()
+        console.log(texto);
     }
 
     return (
         <div>
         <h1>Bienvenido a ANY-TWITTER</h1>
         <h2>Inicia sesión</h2>
-        <form action="#">
-        <label htmlFor="Inicio_correo">Correo electrónico: </label>
+        <p>Correo electrónico: </p>
         <input type="email" name="Inicio_correo" id="Inicio_correo" value={correo} onChange={ (e) => setCorreo(e.target.value) }/><br/>
-        <label htmlFor="Inicio_clave">Contraseña: </label>
+        <p>Contraseña: </p>
         <input type="password" name="Inicio_clave" id="Inicio_clave" value={clave} onChange={ (e) => setClave(e.target.value) }/>
         <br />
-        <button type="submit" onClick={Inicio_iniciar_sesion}> Iniciar sesión</button> 
-        </form>
+        <button onClick={Inicio_iniciar_sesion}> Iniciar sesión</button> 
         <br />
         <h2>No tiene cuenta?</h2>
         <button type="button" >Registrese aquí</button>
