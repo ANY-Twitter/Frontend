e1fb061feb44be151bf78f7dbc8124e06c1d8661
Prober76 <ugartegrover@gmail.com>
2023-06-17 00:38:55 -0500

    .

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,9 +1,19 @@
 import Tweet from "./Tweet";
 import '../styles/Tweets.css'
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 
 function Tweets(props) {
+
+    useEffect( () => {
+        fetch("http://localhost:8000/obtenerMensajes")
+        .then(r => r.blob())
+        .then(r => r.arrayBuffer())
+        .then(r => console.log(new Uint8Array(r)));
+        
+
+    })
 
     const {tweets} = props;
 
