133fff0d67815985f3baee19df07846677d28b80
Eric <eric.bracamonte@utec.edu.pe>
2023-07-06 16:01:35 -0500

    added create tweets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,65 +7,39 @@ import NewTweet from './NewTweet';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from './Contexts.jsx';
 
-function createSampleTweets(n) {
-    let colection = [];
-    let dummy_data = {
-        name: 'Eric Bracamonte',
-        handle: 'Ereiclo',
-        data: `Lorem Ipsum is simply dummy text of the 
-                printing and typesetting industry. Lorem Ipsum 
-                has been the industry Lorem Ipsum is simply dummy text of the 
-                printing and typesetting industry. `,
-        srcImg: user_photo,
-        isTweet: false,
-    };
 
-    for (let i = 0; i < n; ++i) {
-        let actual_dummy_data = { ...dummy_data };
-        actual_dummy_data.id = i;
-        colection.push(actual_dummy_data);
-    }
-
-    return colection;
-
-}
 
 function Home() {
     const user = useContext(UserContext);
 
-    const sampleTweets = createSampleTweets(10);
-    const [tweets,setTweets] = useState([]);
 
-    useEffect(()=>{
-        const load_tweets = async () =>{
+    const [tweets, setTweets] = useState([]);
+    const load_tweets = async () => {
 
-            console.log('aaa')
-            const tweets_response = await fetch('http://localhost:8000/obtenerTweets');
-            const tweets = await tweets_response.json();
+        const tweets_response = await fetch('http://localhost:8000/obtenerTweets');
+        const tweets = await tweets_response.json();
 
 
-            setTweets(tweets.map(({id,data,usuario: [{handle,name,pictureName}]},index) => {
-                console.log('La data es: ',data);
-                console.log('El usuario es: ',name);
-                console.log('El handle es: ',handle);
-                console.log('La picture name es: ',pictureName);
-                //const endPointImage ='';
+        setTweets(tweets.map(({ id, data, usuario: [{ handle, name, pictureName }] }, index) => {
+            console.log('La data es: ', data);
+            console.log('El usuario es: ', name);
+            console.log('El handle es: ', handle);
+            console.log('La picture name es: ', pictureName);
+            //const endPointImage ='';
 
-                return {id,name,handle,data,srcImg:''};
+            return { id, name, handle, data, srcImg: '' };
 
-            }))
+        }))
 
 
- 
-        }
+
+    }
+
+    useEffect(() => {
 
         load_tweets();
-       
 
-        
-
-
-    },[])
+    }, [])
 
 
     return (
@@ -73,9 +47,9 @@ function Home() {
 
             <h2>Home</h2>
             <div className="create-tweet">
-                <NewTweet/>
+                <NewTweet load_tweets={load_tweets}/>
             </div>
-            <Tweets tweets={tweets}/>
+            <Tweets tweets={tweets} />
         </div>
     )
 
--- src/components/NewTweet.jsx
+++ src/components/NewTweet.jsx
@@ -3,12 +3,36 @@ import default_photo from "../img/default.jpg";
 import user_photo from "../img/test-username-photo.jpeg";
 import '../styles/NewTweet.css'
 import './Contexts.jsx'
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "./Contexts.jsx";
 
-function NewTweet(props){
+function NewTweet({load_tweets}){
 
+    const [tweetData,setTweetData] = useState("");
     const user = useContext(UserContext);
+
+    const updateTweetData = (e) => {
+        setTweetData(e.currentTarget.value);
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+
+        const response = await fetch('http://127.0.0.1:8000/tweet',{
+            method: 'POST',
+            body: JSON.stringify({
+                data: tweetData,
+                handle: user.handle,
+                date: new Date().toISOString(),
+            }),
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        });
+
+        load_tweets();
+    }
 
 
 
@@ -18,9 +42,9 @@ function NewTweet(props){
                 <img src={default_photo} alt="" />
             </div>
             <div className="form-section">
-                <form action="" noValidate>
-                    <textarea name="new_tweet_data" id="new_tweet_data" cols="30" rows="10" placeholder="¿Qué estas pensando?"></textarea>
-                    <Link className="button">Crear</Link>
+                <form action="" noValidate onSubmit={submit}>
+                    <textarea onChange={(e) => updateTweetData(e) } value={tweetData} name="new_tweet_data" id="new_tweet_data" cols="30" rows="10" placeholder="¿Qué estas pensando?"></textarea>
+                    <button className="button">Crear</button>
                 </form>
             </div>
         </div>
